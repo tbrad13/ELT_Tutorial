3d041700ef755209ecf9cff544075f28aa1f03e0
Thomas Coker <tcokerbaylor@gmail.com>
2025-12-29 14:46:16 -0600

    added data_loading.py script to load data from the json files for prep into the data warehouse

---
+++ dags/datawarehouse/data_loading.py
@@ -0,0 +1,25 @@
+import json
+from datetime import date
+import logging
+
+
+# Define Logger Object
+logger = logging.getLogger(__name__)
+
+def load_path():
+
+    file_path = f"./data/MrBeastYT_data_{date.today()}.json"
+
+    try:
+        logger.info(f"Processing file: MrBeastYT_data{date.today()}")
+
+        with open(file_path, 'r', encoding='utf-8') as raw_data:
+            data = json.load(raw_data)
+        
+        return data
+    except FileNotFoundError:
+        logger.error(f"File not found: {file_path}")
+        raise
+    except json.JSONDecodeError:
+        logger.error(f"Invalid JSON in file {file_path}")
+        raise
